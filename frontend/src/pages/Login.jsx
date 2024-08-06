e3d030bea34e8c2cdc25873e38d27c5d52cd5594
NymeriaStark81 <phantramy.nguyensieu@gmail.com>
2024-08-06 15:37:59 +0700

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
     const handleSubmit = (e) => {
         try {
             e.preventDefault();
-            fetch("https://test4-gamma-two.vercel.app/login/", {
+            fetch("http://localhost:3000/login/", {
                 method: 'POST',
                 body: JSON.stringify({email, password}),
                 headers: {
