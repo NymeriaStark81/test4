a48ef8ab95e7162fd57ac6e31eab6ff1e6f1fe8e
NymeriaStark81 <phantramy.nguyensieu@gmail.com>
2024-08-07 11:43:36 +0700

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     const handleSubmit = (e) => {
         try {
             e.preventDefault();
-            fetch("https://test4-one-zeta.vercel.app/login", {
+            fetch("/login", {
                 method: 'POST',
                 body: JSON.stringify({email, password}),
                 headers: {
