0dec3333bf682d9613b33bf947e325a3ba10b631
NymeriaStark81 <phantramy.nguyensieu@gmail.com>
2024-08-15 11:45:28 +0700

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     const handleSubmit = (e) => {
         try {
             e.preventDefault();
-            fetch("http://test4-dun-one.vercel.app/login", {
+            fetch("https://test4-beta-seven.vercel.app/login", {
                 method: 'POST',
                 body: JSON.stringify({email, password}),
                 headers: {
